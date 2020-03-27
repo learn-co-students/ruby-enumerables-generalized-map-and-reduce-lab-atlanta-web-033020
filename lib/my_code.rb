f8b4653655a9a5da726504493405a14be4e4b97f
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-27 01:50:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  count = 0
+  
+  while count < array.length
+    new_array << yield(array[count])
+    count += 1
+  end
+  new_array
+end
+
+def reduce(array, src = 0)
+  value = src
+  count = 0
+  while count < array.length do
+    value = yield(array[count], value)
+    value = false if value == nil
+    count += 1
+  end
+  value
+end
